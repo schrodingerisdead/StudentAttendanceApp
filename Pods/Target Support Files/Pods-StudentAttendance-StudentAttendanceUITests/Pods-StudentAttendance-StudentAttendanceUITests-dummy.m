22afde27671373257166d4149443cca4fbd1e391
Magdalena Oreshkova <oreskovam7@gmail.com>
2023-12-28 22:07:04 +0100

    changes

---
+++ Pods/Target Support Files/Pods-StudentAttendance-StudentAttendanceUITests/Pods-StudentAttendance-StudentAttendanceUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StudentAttendance_StudentAttendanceUITests : NSObject
+@end
+@implementation PodsDummy_Pods_StudentAttendance_StudentAttendanceUITests
+@end
